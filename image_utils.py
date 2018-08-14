5e5358e8e949de0f28661e45deca85c66bde8198
Marciano Saraiva <marciano@agrosatelite.com.br>
2018-08-14 02:08:51 -0300

    update model

--- image_utils.py
+++ image_utils.py
@@ -3,7 +3,7 @@
 import os
 import numpy as np
 from osgeo import gdal
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 from sklearn import decomposition
 from skimage.util import img_as_float
 from skimage.filters import sobel
@@ -11,75 +11,73 @@ from skimage.transform import resize, rotate
 from skimage import exposure
 from tqdm import tqdm
 
-SCALE_LEVEL = 0.60
-
 
 def load_file(path, width=None, height=None):
-	dataSource = gdal.Open(path)
+    dataSource = gdal.Open(path)
 
-	bands = []
-	for index in range(1, dataSource.RasterCount + 1):
-		band = dataSource.GetRasterBand(index).ReadAsArray()
-		band_min = np.min(band)
-		band_max = np.max(band)
-		band_normalized = (2 * ((band - band_min) / float(band_max - band_min))) - 1
-		bands.append(band_normalized)
+    bands = []
+    for index in range(1, dataSource.RasterCount + 1):
+        band = dataSource.GetRasterBand(index).ReadAsArray()
+        band_min = np.min(band)
+        band_max = np.max(band)
+        band_normalized = (2 * ((band - band_min) / float(band_max - band_min))) - 1
+        bands.append(band_normalized)
 
-	image = np.dstack(bands)
+    image = np.dstack(bands)
 
-	if width and height:
-		image = resize(image, (width, height))
+    if width and height:
+        image = resize(image, (width, height))
 
-	# plt.imshow(image[: , : , 0:3])
-	# plt.show()
+    # plt.imshow(image[: , : , 0:3])
+    # plt.show()
 
-	return image
+    return image
 
 
 def dargumentation(image):
-	images = []
-	for rot in [0, 90, 180, 270]:
-		images.append(rotate(image, rot))
-	return images
+    images = []
+    for rot in [0, 90, 180, 270]:
+        images.append(rotate(image, rot))
+    return images
 
 
 def load_data(directory, labels, width, height, extension):
-	npz_path = directory + '.npz'
+    npz_path = directory + '.npz'
 
-	if os.path.isfile(npz_path):
-		print("Reading from cache " + npz_path + "...")
-		data = np.load(npz_path)
-		input_data = list(zip(*data['input']))
+    if os.path.isfile(npz_path):
+        print("Reading from cache " + npz_path + "...")
+        data = np.load(npz_path)
+        input_data = list(zip(*data['input']))
 
-		x = np.array(input_data[0])
-		y = np.array(input_data[1])
+        x = np.array(input_data[0])
+        y = np.array(input_data[1])
 
-		return x, y
-	else:
-		x = []
-		y = []
-		for label, label_value in labels.items():
-			label_directory = "{root_path}/{label}".format(root_path=directory, label=label)
-			files = [f for f in os.listdir(label_directory) if os.path.isfile(os.path.join(label_directory, f)) and f.endswith(extension)]
-			for f in tqdm(files, miniters=10):
-				path = "{directory}/{file}".format(directory=label_directory, file=f)
-				image = load_file(path, width, height)
-				
-				for i in dargumentation(image):
-					x.append(i)
-					y.append(label_value)
+        return x, y
+    else:
+        x = []
+        y = []
+        for label, label_value in labels.items():
+            label_directory = "{root_path}/{label}".format(root_path=directory, label=label)
+            files = [f for f in os.listdir(label_directory) if os.path.isfile(os.path.join(label_directory, f)) and f.endswith(extension)]
+            for f in tqdm(files, miniters=10):
+                path = "{directory}/{file}".format(directory=label_directory, file=f)
+                image = load_file(path, width, height)
+                for i in dargumentation(image):
+                    x.append(i)
+                    y.append(label_value)
 
-		x = np.array(x, np.float32)
-		y = np.array(y, np.uint8)
-		data = np.array(list(zip(x, y)))
-		np.savez_compressed(npz_path, input=data)
-		return x, y
+        x = np.array(x, np.float32)
+        y = np.array(y, np.uint8)
+        data = np.array(list(zip(x, y)))
+        np.savez_compressed(npz_path, input=data)
+        return x, y
+
 
 def sliding_window(image, stepSize, windowSize):
-	# slide a window across the image
-	for y in range(0, image.shape[0], stepSize):
-		for x in range(0, image.shape[1], stepSize):
-			# yield the current window
-			window = image[y:y + windowSize[1], x:x + windowSize[0]]
-			# window = resize(window, (windowSize[0], windowSize[1]))
-			yield (x, y, window)
+    # slide a window across the image
+    for y in range(0, image.shape[0], stepSize):
+        for x in range(0, image.shape[1], stepSize):
+            # yield the current window
+            window = image[y:y + windowSize[1], x:x + windowSize[0]]
+            # window = resize(window, (windowSize[0], windowSize[1]))
+            yield (x, y, window)
--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 # -*- encoding=UTF-8-*-
 
 import numpy as np
-import os, sys
+import sys
 from skimage.util import img_as_float
 import matplotlib.patches as patches
 from matplotlib import pyplot as plt
 
-from tensorflow.python.keras.callbacks import TensorBoard
 import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
@@ -15,146 +14,144 @@ from skimage import io, exposure
 import models
 import image_utils
 
-tensorboard = TensorBoard(log_dir='logs', histogram_freq=10)
-
-image_width = 256
-image_height = 256
+image_width = 128
+image_height = 128
 channels = 4
 extension = '.tif'
 labels = {
-	'pivot': 0,
-	'infra': 1,
+    'pivot': 0,
+    'infra': 1,
+    'infra2': 2,
 }
 num_classes = len(labels)
 weights_path = "weights"
 
 
-def train(model):
-	epochs = 100
-	batch_size = 2
-	features, target = image_utils.load_data("data/train", labels, image_width, image_height, extension)
-	train_data, eval_data, train_labels, eval_labels = train_test_split(features, target, test_size=0.2)
+def train(model, epochs=100, batch_size=20):
+    features, target = image_utils.load_data("data/train", labels, image_width, image_height, extension)
+    train_data, eval_data, train_labels, eval_labels = train_test_split(features, target, test_size=0.2)
 
-	print('Split train: ', len(train_data))
-	print('Split test: ', len(eval_data))
+    print('Split train: ', len(train_data))
+    print('Split test: ', len(eval_data))
 
-	classifier = tf.estimator.Estimator(model_fn=model, model_dir=weights_path)
-	logging_hook = tf.train.LoggingTensorHook(tensors={"probabilities": "softmax_tensor"}, every_n_iter=50)
+    classifier = tf.estimator.Estimator(model_fn=model, model_dir=weights_path)
+    logging_hook = tf.train.LoggingTensorHook(tensors={"probabilities": "softmax_tensor"}, every_n_iter=50)
 
-	train_input_fn = tf.estimator.inputs.numpy_input_fn(
-		x={"x": train_data},
-		y=np.asarray(train_labels, dtype=np.int32),
-		batch_size=batch_size,
-		num_epochs=1,
-		shuffle=True
-	)
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_data},
+        y=np.asarray(train_labels, dtype=np.int32),
+        batch_size=batch_size,
+        num_epochs=1,
+        shuffle=True
+    )
 
-	eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-		x={"x": eval_data},
-		y=np.asarray(eval_labels, dtype=np.int32),
-		num_epochs=1,
-		shuffle=False)
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=np.asarray(eval_labels, dtype=np.int32),
+        num_epochs=1,
+        shuffle=False)
 
-	for epoch in range(1, epochs + 1):
-		print("EPOCH:", epoch)
-		classifier.train(input_fn=train_input_fn, steps=20000, hooks=[logging_hook])
-		eval_results = classifier.evaluate(input_fn=eval_input_fn)
-		print(eval_results)
-
-
+    for epoch in range(1, epochs + 1):
+        print("EPOCH:", epoch)
+        classifier.train(input_fn=train_input_fn, steps=20000, hooks=[logging_hook])
+        eval_results = classifier.evaluate(input_fn=eval_input_fn)
+        print(eval_results)
 
 def evaluate(model):
-	validation_features, validation_target = image_utils.load_data("data/validation", labels, image_width, image_height, extension)
+    validation_features, validation_target = image_utils.load_data("data/validation", labels, image_width, image_height, extension)
 
-	classifier = tf.estimator.Estimator(model_fn=model, model_dir=weights_path)
+    classifier = tf.estimator.Estimator(model_fn=model, model_dir=weights_path)
 
-	validation_input_fn = tf.estimator.inputs.numpy_input_fn(
-		x={"x": validation_features},
-		y=np.asarray(validation_target, dtype=np.int32),
-		num_epochs=1,
-		shuffle=False)
+    validation_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": validation_features},
+        y=np.asarray(validation_target, dtype=np.int32),
+        num_epochs=1,
+        shuffle=False)
 
-	validation_results = classifier.evaluate(input_fn=validation_input_fn)
-	print(validation_results)
+    validation_results = classifier.evaluate(input_fn=validation_input_fn)
+    print(validation_results)
+
 
 def predict(model, path):
-	stepSize = 100
-	batch_size = 1
+    stepSize = 100
+    batch_size = 20
 
-	classifier = tf.estimator.Estimator(model_fn=model, model_dir=weights_path)
+    classifier = tf.estimator.Estimator(model_fn=model, model_dir=weights_path)
 
-	image = image_utils.load_file(path)
-	image = image[:, :, :channels]
+    image = image_utils.load_file(path)
+    image = image[:, :, :channels]
 
-	plt.ion()
-	fig, ax = plt.subplots(1)
-	image_rgb = img_as_float(io.imread(path))
-	image_rgb = image_rgb[:, :, :3]
-	image_rgb = exposure.rescale_intensity(image_rgb)
+    plt.ion()
+    fig, ax = plt.subplots(1)
+    image_rgb = img_as_float(io.imread(path))
+    image_rgb = image_rgb[:, :, :3]
+    image_rgb = exposure.rescale_intensity(image_rgb)
 
-	mng = plt.get_current_fig_manager()
-	mng.resize(*mng.window.maxsize())
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
 
-	ax.imshow(image_rgb)
+    ax.imshow(image_rgb)
 
-	batch = []
-	count = 1
+    batch = []
+    count = 1
 
-	for (x, y, window) in image_utils.sliding_window(image, stepSize=stepSize, windowSize=(image_width, image_height)):
+    for (x, y, window) in image_utils.sliding_window(image, stepSize=stepSize, windowSize=(image_width, image_height)):
 
-		if window.shape[0] != image_height or window.shape[1] != image_width:
-			print(x, y, window.shape)
-			continue
+        if window.shape[0] != image_height or window.shape[1] != image_width:
+            continue
 
-		batch.append({
-			"window": window,
-			"x": x,
-			"y": y
-		})
+        batch.append({
+            "window": window,
+            "x": x,
+            "y": y
+        })
 
-		if len(batch) >= batch_size:
-			fig.savefig('results/{0}.png'.format(count))
-			count += 1
+        if len(batch) >= batch_size:
+            fig.savefig('results/{0}.png'.format(count))
+            count += 1
 
-			windows = []
-			positions = []
-			for b in batch:
-				windows.append(b.get("window"))
-				positions.append((b.get("x"), b.get("y")))
-			windows = np.array(windows)
+            windows = []
+            positions = []
+            for b in batch:
+                windows.append(b.get("window"))
+                positions.append((b.get("x"), b.get("y")))
+            windows = np.array(windows)
 
-			predict_input_fn = tf.estimator.inputs.numpy_input_fn(
-				x={"x": windows},
-			)
+            predict_input_fn = tf.estimator.inputs.numpy_input_fn(
+                x={"x": np.asarray(windows, dtype=np.float32)},
+                shuffle=False,
+                num_epochs=1,
+            )
 
-			pred = classifier.predict(input_fn=predict_input_fn)
-			print(pred)
-			pred_prob = [0]
+            results = classifier.predict(input_fn=predict_input_fn)
+            pred = []
+            pred_prob = []
+            for p in results:
+                pred.append(p["classes"])
+                pred_prob.append(p["probabilities"])
 
-			for window, position, label, prob in zip(windows, positions, pred, pred_prob):
-				label_class = labels.keys()[labels.values().index(label)]
-				label_prob = prob[label]
+            for window, position, label, prob in zip(windows, positions, pred, pred_prob):
+                prediction = prob[np.argmax(prob)]
+                print("Label: {0} --> Prediction: {1}".format(label, prediction))
 
-				print
-				"Label: {0} --> {1}".format(label, label_prob)
+                if label == labels.get("pivot"): # and prediction > 0.90:
+                    rect = patches.Rectangle((position[0], position[1]), image_width, image_height, linewidth=1,
+                                             edgecolor='r', color='y', facecolor='none')
+                else:
+                    rect = patches.Rectangle((position[0], position[1]), image_width, image_height, linewidth=1,
+                                             edgecolor='r', facecolor='none')
 
-				if label == labels.get("pivot"):  # and label_prob > 0.90:
-					rect = patches.Rectangle((position[0], position[1]), image_width, image_height, linewidth=1,
-											 edgecolor='r', color='y', facecolor='none')
-				else:
-					rect = patches.Rectangle((position[0], position[1]), image_width, image_height, linewidth=1,
-											 edgecolor='r', facecolor='none')
+                ax.add_patch(rect)
 
-				ax.add_patch(rect)
+            batch = []
 
-			batch = []
+        # window = window[-1][:,:,:3]
+        # print window.shape
+        # plt.axis("off")
+        # plt.imshow(window.reshape((window.shape[0], window.shape[1])), cmap=plt.cm.nipy_spectral, interpolation='nearest')
+        # plt.show()
+        fig.canvas.draw()
 
-		# window = window[-1][:,:,:3]
-		# print window.shape
-		# plt.axis("off")
-		# plt.imshow(window.reshape((window.shape[0], window.shape[1])), cmap=plt.cm.nipy_spectral, interpolation='nearest')
-		# plt.show()
-		fig.canvas.draw()
 
 mode = sys.argv[1]
 
@@ -163,12 +160,12 @@ config = tf.ConfigProto(gpu_options=gpu_options)
 config.gpu_options.allow_growth = True
 
 with tf.Session(config=config) as sess:
-	model = models.model_fn
+    model = models.get_model(image_width, image_height, channels, num_classes)
 
-	if mode == 'train':
-		train(model)
-	elif mode == 'evaluate':
-		evaluate(model)
-	elif mode == 'predict':
-		file_path = sys.argv[2]
-		predict(model, file_path)
+    if mode == 'train':
+        train(model)
+    elif mode == 'evaluate':
+        evaluate(model)
+    elif mode == 'predict':
+        file_path = sys.argv[2]
+        predict(model, file_path)
